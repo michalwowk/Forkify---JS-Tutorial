98bf25a2badacd454777ad050657a2a165579789
unknown <michalwowk92@gmail.com>
2018-12-30 10:58:39 +0100

    Webpack configured

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 // Global app controller
+import test from './test';
+const x = 43
+console.log(`${test} asdasda. variable x is ${x}`);
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+console.log('rampampam');
+
+export default 25;
